9c0d48850b5614323e475168cbf31f70b436e324
virtyaluk <bohdan@modern-dev.com>
2018-10-30 02:40:01 +0200

    Adds consumer and producer dotnet projects.

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace KafkaConsumer
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var config = new Dictionary<string, object>
+      {
+          { "group.id", "sample-consumer" },
+          { "bootstrap.servers", "localhost:9092" },
+          { "enable.auto.commit", "false"}
+      };
+
+            using (var consumer = new Consumer<Null, string>(config, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                consumer.OnError += (_, e) => Console.WriteLine($"Error: {e.Reason}");
+                consumer.OnMessage += (_, msg) =>
+                {
+                    Console.WriteLine($"Topic: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
+                    consumer.CommitAsync(msg);
+                };
+                
+                consumer.Subscribe(new [] { "hello-topic" });
+
+                var cancelled = false;
+
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cancelled = true;
+                };
+                
+                Console.WriteLine("Ctrl-C to exit.");
+
+                while (!cancelled)
+                {
+                    consumer.Poll(100);
+                }
+            }
+        }
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace KafkaProducer
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var config = new Dictionary<string, object>
+            {
+                { "bootstrap.servers", "localhost:9092" }
+            };
+
+            using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+            {
+                producer.OnError += (_, e) => Console.WriteLine($"Error: {e.Reason}");
+                
+                string text = null;
+
+                while (text != "exit")
+                {
+                    text = Console.ReadLine();
+                    producer.ProduceAsync("hello-topic", null, text);
+                }
+
+                producer.Flush(100);
+            }
+        }
+    }
+}
